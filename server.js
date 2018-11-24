876b2151547ac30eea81dcde02eb523e6eba4928
sonhong <sonbui151097@gmail.com>
2018-11-24 23:22:19 +0700

    Create and defind by passport and express

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var flash = require('flash');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+
+var config = require('./config/db');
+var port = process.env.PORT || 3000;
+mongoose.connect(config.db, {
+    useMongoClient: true
+});
+
+require('./config/passport')(passport);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(session({
+    secret: 'secret123',
+    saveUninitialized:true,
+    resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+require('./routes/user.route')(app, passport);
+
+app.listen(port);
